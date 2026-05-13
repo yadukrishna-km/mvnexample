56f72d70bfcf9b680232deb73d38d0003cc88003
yadukrishna-km <yadukrishnapilikudlu@gmail.com>
2026-05-13 07:19:04 +0530

    first commit

---
+++ src/main/java/lab8/mvnproj/App.java
@@ -0,0 +1,13 @@
+package lab8.mvnproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
